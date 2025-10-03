9593675baf2f19fa6a7a433fc735f7115c6e9b69
Andrew Kew <andrewkew@gmail.com>
2025-10-03 12:12:59 +0100

    Ignore tfvars and removing commented out content

--- terraform/portal.tf
+++ terraform/portal.tf
@@ -1,24 +1,3 @@
-# resource "konnect_portal_auth" "my_portalauth" {
-#   basic_auth_enabled      = true
-#   idp_mapping_enabled     = true
-#   konnect_mapping_enabled = false
-#   oidc_auth_enabled       = false
-#   oidc_claim_mappings = {
-#     email  = "email"
-#     groups = "custom-group-claim"
-#     name   = "name"
-#   }
-#   oidc_client_id     = "...my_oidc_client_id..."
-#   oidc_client_secret = "...my_oidc_client_secret..."
-#   oidc_issuer        = "...my_oidc_issuer..."
-#   oidc_scopes = [
-#     "..."
-#   ]
-#   oidc_team_mapping_enabled = true
-#   portal_id                 = "f32d905a-ed33-46a3-a093-d8f536af9a8a"
-#   saml_auth_enabled         = false
-# }
-
 resource "konnect_portal" "my_portal" {
   authentication_enabled               = false
   auto_approve_applications            = false
