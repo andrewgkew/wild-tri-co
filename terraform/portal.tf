9713dec6c8ff6097bd5fc9f0a04eb2fa780af6d4
Andrew Kew <andrewkew@gmail.com>
2025-10-06 10:01:56 +0100

    Turning on authenticationa and auto approval

--- terraform/portal.tf
+++ terraform/portal.tf
@@ -1,7 +1,7 @@
 resource "konnect_portal" "my_portal" {
-  authentication_enabled               = false
-  auto_approve_applications            = false
-  auto_approve_developers              = false
+  authentication_enabled               = true
+  auto_approve_applications            = true
+  auto_approve_developers              = true
   default_api_visibility               = "public"
   default_application_auth_strategy_id = "dee3f180-36c3-44bb-8567-6d32e08387e9"
   default_page_visibility              = "public"
